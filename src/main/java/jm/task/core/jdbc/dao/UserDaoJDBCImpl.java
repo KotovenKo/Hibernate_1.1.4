cba8803c04376b08420c61a679bcfd34025917a1
Vitaly <kotovenko.vitaly@gmail.com>
2022-02-17 19:57:41 +0300

    Relocation methods to UserDaoJDBCImpl.class

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +16,94 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+       try {
+           Statement statement = Util.getConnection().createStatement();
+           // TABLE CREATION
+           statement.execute("CREATE TABLE if not exists katausers (id int not null AUTO_INCREMENT, " +
+                   "name varchar(40), " +
+                   "secondName varchar(40), " +
+                   "age int, " +
+                   "primary key (id))");
+
+       } catch (SQLException e) {
+           System.out.println("Ошибка создания таблицы");
+       }
 
     }
 
     public void dropUsersTable() {
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.execute("DROP TABLE katausers");
+        } catch (SQLException e) {
+            System.out.println("Ошибка удаления таблицы");
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+      /*  PreparedStatement preparedStatement;
+       try {
+           preparedStatement = Util.getConnection().prepareStatement("insert into katausers (name, secondName, age) values (?,?,?)");
+
+           preparedStatement.setString(1, name);
+           preparedStatement.setString(2, lastName);
+           preparedStatement.setInt(3, age);
+           preparedStatement.execute();
+           System.out.printf("User с именем %s добавлен в базу данных\n", name);
+       } catch (SQLException e) {
+           System.out.println("Ошибка добавления Юзера в БД");
+       }*/
+
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.execute("INSERT INTO katausers (name, secondName, age) VALUES ('"+name+"', '"+lastName+"', '"+age+"')");
+            System.out.printf("User с именем %s добавлен в базу данных\n", name);
+        } catch (SQLException e) {
+            System.out.println("Ошибка удаления таблицы");
+        }
 
     }
 
+
+
+
     public void removeUserById(long id) {
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            statement.execute("DELETE FROM katausers where id = '"+id+"'");
+        } catch (SQLException e) {
+            System.out.println("Ошибка удаления юзера по ID");
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            ResultSet resultSettoList = statement.executeQuery("select * from katausers");
+            while (resultSettoList.next()) {
+                userList.add(new User(resultSettoList.getString("name"),
+                        resultSettoList.getString("secondName"),
+                        (byte) resultSettoList.getInt("age")));
+            }
+        } catch (SQLException e) {
+            System.out.println("Ошибка сохранения всех Юзеров");
+        }
+        return userList;
+
     }
 
     public void cleanUsersTable() {
+        try {
+            Statement statement = Util.getConnection().createStatement();
+            // TABLE CREATION
+            statement.execute("TRUNCATE TABLE katausers");
+
+        } catch (SQLException e) {
+            System.out.println("Ошибка очистки таблицы");
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,26 +1,19 @@
 package jm.task.core.jdbc.util;
 
-import java.security.PublicKey;
 import java.sql.*;
 
 public class Util {
-    private final String URL = "jdbc:mysql://localhost:3306/javadb";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "1234";
-    Connection connection;
+    private static final String URL = "jdbc:mysql://localhost:3306/javadb";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
 
-    public Connection getConnection() {
-        return connection;
-    }
+    public static Connection connection;
 
-    public Util() {
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Соединение установлено!!!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        if (!connection.isClosed()) {
+            System.out.println("Соединение установлено!!!");
         }
+        return connection;
     }
 }
