91f92c2413d6714a3da3fe894f61814f475c04a6
Vitaly <kotovenko.vitaly@gmail.com>
2022-02-17 08:09:28 +0300

    Add DB connection. Add USERS creation

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,69 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util util = new Util();
+        //USER CREATION
+        User user1 = new User("Chack", "Norris", (byte) 45);
+        User user2 = new User("Jack", "Nicolson", (byte) 50);
+        User user3 = new User("Jan", "Vandam", (byte) 55);
+        User user4 = new User("Bruce", "Lee", (byte) 45);
+
+
+        try {
+            Statement statement = util.getConnection().createStatement();
+            // TABLE CREATION
+            statement.execute("CREATE TABLE if not exists katausers (id int not null AUTO_INCREMENT, " +
+                    "name varchar(40), " +
+                    "secondName varchar(40), " +
+                    "age int, " +
+                    "primary key (id))");
+            statement.execute("insert into katausers (name, secondName, age) values ('" + user1.getName() + "', '" + user1.getLastName() + "', '" + user1.getAge() + "')");
+            System.out.printf("User с именем %s добавлен в базу данных\n", user1.getName());
+            statement.execute("insert into katausers (name, secondName, age) values ('" + user2.getName() + "', '" + user2.getLastName() + "', '" + user2.getAge() + "')");
+            System.out.printf("User с именем %s добавлен в базу данных\n", user2.getName());
+            statement.execute("insert into katausers (name, secondName, age) values ('" + user3.getName() + "', '" + user3.getLastName() + "', '" + user3.getAge() + "')");
+            System.out.printf("User с именем %s добавлен в базу данных\n", user3.getName());
+            statement.execute("insert into katausers (name, secondName, age) values ('" + user4.getName() + "', '" + user4.getLastName() + "', '" + user4.getAge() + "')");
+            System.out.printf("User с именем %s добавлен в базу данных\n", user4.getName());
+
+            ResultSet resultSet = statement.executeQuery("select * from katausers");
+            while (resultSet.next()) {
+               User newUser1 = new User(resultSet.getString("name"), resultSet.getString("secondName"), resultSet.getByte("age"));
+                System.out.println(newUser1);
+/*               User newUser2 = new User(resultSet.getString("name"), resultSet.getString("secondName"), resultSet.getByte("age"));
+                System.out.println(newUser2);
+               User newUser3 = new User(resultSet.getString("name"), resultSet.getString("secondName"), resultSet.getByte("age"));
+                System.out.println(newUser3);
+               User newUser4 = new User(resultSet.getString("name"), resultSet.getString("secondName"), resultSet.getByte("age"));
+                System.out.println(newUser4);*/
+            }
+
+            statement.execute("TRUNCATE TABLE katausers");
+
+
+
+
+
+
+
+
+
+
+            //TABLE REMOVING
+            //statement.execute("DROP TABLE IF EXISTS katausers");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import java.security.PublicKey;
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final String URL = "jdbc:mysql://localhost:3306/javadb";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "1234";
+    Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Util() {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Соединение установлено!!!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
