012d756feaa7ee87a46aeddc60af4ec7d6665dc0
Vitaly <kotovenko.vitaly@gmail.com>
2022-02-18 21:11:08 +0300

    Add methods in UserServiceImpl. Tests complited

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,26 @@
 package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
         userDaoJDBC.createUsersTable();
+
+
         userDaoJDBC.saveUser("Chuck", "Norris", (byte) 45);
         userDaoJDBC.saveUser("Jack", "Sparrow", (byte) 30);
         userDaoJDBC.saveUser("Bruce", "Lee", (byte) 45);
         userDaoJDBC.saveUser("Janclod", "Vandam", (byte) 30);
-        List<User> user = userDaoJDBC.getAllUsers();
-        for (User u : user) {
+
+       List <User> users = userDaoJDBC.getAllUsers();
+
+        for (User u : users) {
             System.out.println(u);
         }
         userDaoJDBC.cleanUsersTable();
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -16,96 +16,94 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-       try {
-           Statement statement = Util.getConnection().createStatement();
-           // TABLE CREATION
-           statement.execute("CREATE TABLE if not exists katausers (id int not null AUTO_INCREMENT, " +
-                   "name varchar(40), " +
-                   "secondName varchar(40), " +
-                   "age int, " +
-                   "primary key (id))");
+        try (Statement statement = Util.getConnection().createStatement()) {
+            // TABLE CREATION
+            statement.execute("CREATE TABLE IF NOT EXISTS katausers (id BIGINT NOT NULL AUTO_INCREMENT, " +
+                    "name VARCHAR(40) NOT NULL , " +
+                    "lastName VARCHAR(40) NOT NULL , " +
+                    "age SMALLINT NOT NULL , " +
+                    "PRIMARY KEY (id))");
 
-       } catch (SQLException e) {
-           System.out.println("Ошибка создания таблицы");
-       }
+        } catch (SQLException e) {
+            System.out.println("Ошибка создания таблицы");
+            e.printStackTrace();
+        }
 
     }
 
     public void dropUsersTable() {
-        try {
-            Statement statement = Util.getConnection().createStatement();
+        try (Statement statement = Util.getConnection().createStatement()) {
             statement.execute("DROP TABLE katausers");
         } catch (SQLException e) {
             System.out.println("Ошибка удаления таблицы");
+            e.printStackTrace();
         }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-      /*  PreparedStatement preparedStatement;
-       try {
-           preparedStatement = Util.getConnection().prepareStatement("insert into katausers (name, secondName, age) values (?,?,?)");
 
-           preparedStatement.setString(1, name);
-           preparedStatement.setString(2, lastName);
-           preparedStatement.setInt(3, age);
-           preparedStatement.execute();
-           System.out.printf("User с именем %s добавлен в базу данных\n", name);
-       } catch (SQLException e) {
-           System.out.println("Ошибка добавления Юзера в БД");
-       }*/
+        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement("insert into katausers (name, lastName, age) values (?,?,?)")) {
 
-        try {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.execute();
+            System.out.printf("User с именем %s добавлен в базу данных\n", name);
+        } catch (SQLException e) {
+            System.out.println("Ошибка добавления Юзера в БД");
+            e.printStackTrace();
+        }
+
+/*        try {
             Statement statement = Util.getConnection().createStatement();
             statement.execute("INSERT INTO katausers (name, secondName, age) VALUES ('"+name+"', '"+lastName+"', '"+age+"')");
             System.out.printf("User с именем %s добавлен в базу данных\n", name);
         } catch (SQLException e) {
             System.out.println("Ошибка удаления таблицы");
-        }
+        }*/
 
     }
 
 
-
-
     public void removeUserById(long id) {
-        try {
-            Statement statement = Util.getConnection().createStatement();
-            statement.execute("DELETE FROM katausers where id = '"+id+"'");
+        try (Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DELETE FROM katausers where id = '" + id + "'");
         } catch (SQLException e) {
             System.out.println("Ошибка удаления юзера по ID");
+            e.printStackTrace();
         }
 
     }
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
-        try {
-            Statement statement = Util.getConnection().createStatement();
+        try (Statement statement = Util.getConnection().createStatement()) {
             ResultSet resultSettoList = statement.executeQuery("select * from katausers");
             while (resultSettoList.next()) {
                 User user = new User();
                 user.setId(resultSettoList.getLong("ID"));
                 user.setName(resultSettoList.getString("name"));
-                user.setLastName(resultSettoList.getString("secondName"));
+                user.setLastName(resultSettoList.getString("lastName"));
                 user.setAge(resultSettoList.getByte("age"));
                 userList.add(user);
             }
         } catch (SQLException e) {
             System.out.println("Ошибка сохранения всех Юзеров");
+            e.printStackTrace();
         }
         return userList;
 
     }
 
     public void cleanUsersTable() {
-        try {
-            Statement statement = Util.getConnection().createStatement();
+        try (Statement statement = Util.getConnection().createStatement()) {
             // TABLE CREATION
             statement.execute("TRUNCATE TABLE katausers");
 
         } catch (SQLException e) {
             System.out.println("Ошибка очистки таблицы");
+            e.printStackTrace();
         }
 
     }
