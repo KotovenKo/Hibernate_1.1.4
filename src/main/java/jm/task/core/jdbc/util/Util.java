cf18386bf610d88ef5c87322bbe614bf8661e219
Vitaly <kotovenko.vitaly@gmail.com>
2022-02-22 18:43:19 +0300

    Adding MySQL table control methods with usage of Hibernate technology

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -41,7 +41,7 @@ public class Util {
                 properties.put(Environment.PASS, PASSWORD);
                 properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                properties.put(Environment.HBM2DDL_AUTO, "none");
+                properties.put(Environment.HBM2DDL_AUTO, "none"); // Самоятоятельно создает таблицу "create"
                 properties.put(Environment.SHOW_SQL, "true");
                 properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
